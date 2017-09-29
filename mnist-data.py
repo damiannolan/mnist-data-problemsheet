202d5be84f839150dbb9f8e6d59224fe2283a77a
Damian Nolan <damiannolan@gmail.com>
2017-09-29 10:17:19 +0100

    Parsing data files loading into memory

---
+++ mnist-data.py
@@ -0,0 +1,77 @@
+# Adapted from:  https://stackoverflow.com/questions/2872381/how-to-read-a-file-byte-by-byte-in-python-and-how-to-print-a-bytelist-as-a-binar
+
+import gzip
+import numpy as np
+
+def read_labels_from_file(filename):
+    with gzip.open(filename, 'rb') as f:
+        # Get the magic number
+        magic = f.read(4)
+        magic = int.from_bytes(magic, 'big')
+        print("Magic is: ", magic)
+
+        # Get the number of labels
+        nolab = f.read(4)
+        nolab = int.from_bytes(nolab, 'big')
+        print("Number of labels: ", nolab)
+
+        # Read the labels into an appropriate data structure i.e as array
+        labels = [f.read(1) for i in range(nolab)]
+        labels = [int.from_bytes(label, 'big') for label in labels]
+
+        return labels
+
+train_labels = read_labels_from_file('data/train-labels-idx1-ubyte.gz')
+test_labels = read_labels_from_file('data/t10k-labels-idx1-ubyte.gz')
+
+# A function to read the images
+def read_images_from_file(filename):
+    with gzip.open(filename, 'rb') as f:
+        # Get the magic number
+        magic = f.read(4)
+        magic = int.from_bytes(magic, 'big')
+        print("Magic is: ", magic)
+
+        # Get the number of images
+        noimages = f.read(4)
+        noimages = int.from_bytes(noimages, 'big')
+        print("Number of images: ", noimages)
+
+        # Number of rows
+        norows = f.read(4)
+        norows = int.from_bytes(norows, 'big')
+        print("Rows: ", norows)
+
+        # Number of columns
+        nocols = f.read(4)
+        nocols = int.from_bytes(nocols, 'big')
+        print("Columns: ", nocols)
+
+        images = []
+
+        for i in range(noimages):
+            rows = []
+            for j in range(norows):
+                cols = []
+                for k in range(nocols):
+                    cols.append(int.from_bytes(f.read(1), 'big'))
+                rows.append(cols)
+            images.append(rows)
+
+        return images
+
+train_images = read_images_from_file('data/train-images-idx3-ubyte.gz')
+
+for row in train_images[4999]:
+    for col in row:
+        print('.' if col < 128 else '#', end='')
+    print()
+'
+import PIL as pil
+img = train_images[4999]
+img = np.array(img)
+img = pil.fromarray(img)
+img = img.convert('RGB')
+img.show()
+img.save('2.png')
+'
---
+++ rough-work.py
@@ -0,0 +1,63 @@
+# Adapted from
+# https://stackoverflow.com/questions/12902540/read-from-a-gzip-file-in-python
+# https://stackoverflow.com/questions/2872381/how-to-read-a-file-byte-by-byte-in-python-and-how-to-print-a-bytelist-as-a-binar
+
+import gzip
+#import numpy as np
+
+f = gzip.open('data/train-images-idx3-ubyte.gz', 'rb')
+
+# firstbyte = f.read(1)
+# print(firstbyte)
+
+magicNumber = f.read(4)
+print(magicNumber)
+
+print(int.from_bytes(magicNumber, byteorder='big'))
+
+print(int.from_bytes(f.read(4), byteorder='big'))
+print(int.from_bytes(f.read(4), byteorder='big'))
+print(int.from_bytes(f.read(4), byteorder='big'))
+
+image = [[0 for x in range(28)] for y in range(28)]
+
+
+for i in range(28):
+    for j in range(28):
+        image[i][j] = int.from_bytes(f.read(1), byteorder='big')
+
+for i in range(28):
+    for j in range(28):
+        if image[i][j] < 128:
+            print(".", end="")
+        else:
+            print("#", end="")
+
+       # print(image[i][j], end="")
+    print()
+
+for i in range(28):
+    for j in range(28):
+        image[i][j] = int.from_bytes(f.read(1), byteorder='big')
+
+for i in range(28):
+    for j in range(28):
+        if image[i][j] < 128:
+            print(".", end="")
+        else:
+            print("#", end="")
+
+    print()
+
+for i in range(28):
+    for j in range(28):
+        image[i][j] = int.from_bytes(f.read(1), byteorder='big')
+
+for i in range(28):
+    for j in range(28):
+        if image[i][j] < 128:
+            print(".", end="")
+        else:
+            print("#", end="")
+    print()
+f.close()
